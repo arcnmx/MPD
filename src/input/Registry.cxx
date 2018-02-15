3d7a80a68ad21af974731decfb6113553120f776
arcnmx <arcnmx@users.noreply.github.com>
2018-02-15 17:55:39 -0500

    youtube-dl input plugin

--- src/input/Registry.cxx
+++ src/input/Registry.cxx
@@ -32,6 +32,10 @@
 #include "plugins/CurlInputPlugin.hxx"
 #endif
 
+#ifdef ENABLE_YTDL
+#include "plugins/YtdlInputPlugin.hxx"
+#endif
+
 #ifdef ENABLE_FFMPEG
 #include "plugins/FfmpegInputPlugin.hxx"
 #endif
@@ -61,6 +65,9 @@ const InputPlugin *const input_plugins[] = {
 #endif
 #ifdef ENABLE_QOBUZ
 	&qobuz_input_plugin,
+#endif
+#ifdef ENABLE_YTDL
+	&input_plugin_ytdl,
 #endif
 #ifdef ENABLE_CURL
 	&input_plugin_curl,
