4502427e108fd194ceb2b7eb2c528433d67e6f9f
arcnmx <arcnmx@users.noreply.github.com>
2018-04-11 18:45:14 -0400

    EventLoop async ytdl

--- src/playlist/plugins/YtdlPlaylistPlugin.cxx
+++ src/playlist/plugins/YtdlPlaylistPlugin.cxx
@@ -46,7 +46,7 @@ playlist_ytdl_open_uri(const char *uri, Mutex &mutex, Cond &cond)
 	auto handle = parser.CreateHandle();
 	/* TODO: PlaylistMode::FLAT is much faster but does not retrieve
 	 * per-song metadata. We should use it instead with RemoteTagScanner */
-	Ytdl::Invoke(*handle, uri, Ytdl::PlaylistMode::FULL);
+	Ytdl::BlockingInvoke(*handle, uri, Ytdl::PlaylistMode::FULL);
 
 	// TODO: sites that don't have expiring URLs don't need the input plugin
 
