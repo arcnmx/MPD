b80c32c1167c61593908f88451d0307ddb28f43f
arcnmx <arcnmx@users.noreply.github.com>
2020-11-06 12:56:29 -0800

    HACK: AbandonFD() is not fork-safe

--- src/event/SocketEvent.cxx
+++ src/event/SocketEvent.cxx
@@ -44,17 +44,8 @@ SocketEvent::Close() noexcept
 		return;
 
 	if (std::exchange(scheduled_flags, 0) != 0)
-		loop.AbandonFD(*this);
+		loop.RemoveFD(fd.Get(), *this);
 	fd.Close();
-}
-
-void
-SocketEvent::Abandon() noexcept
-{
-	if (std::exchange(scheduled_flags, 0) != 0)
-		loop.AbandonFD(*this);
-
-	fd = SocketDescriptor::Undefined();
 }
 
 bool
